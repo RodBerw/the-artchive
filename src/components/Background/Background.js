2ccbb573b578a47823ab3103333aa51c786c12b2
Rodolfo <rodolfo.b.liberado@gmail.com>
2023-09-15 18:03:23 -0300

    About update 2

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -3,7 +3,7 @@ import { Box, Image } from "@chakra-ui/react";
 export default function Background(){
     return(
         <div>
-            <Image w="100%" h="100vh" position="fixed" objectFit="cover" src="/assets/home-background.jpg" zIndex="-99"/>
+            <Image filter="brightness(50%)" w="100%" h="100vh" position="fixed" objectFit="cover" src="/assets/home-background.jpg" zIndex="-99"/>
         </div>
     )
 }
