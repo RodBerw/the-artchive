7b4c6aa4993c9723c20769d34df6db666b0f4584
Rodolfo <rodolfo.b.liberado@gmail.com>
2023-09-25 21:48:22 -0300

    Starting json database tests

--- src/components/Artists/ArtistCard.js
+++ src/components/Artists/ArtistCard.js
@@ -1,9 +1,12 @@
 import { Box, Square, Flex, Text, Grid } from "@chakra-ui/react";
 
-export default function ArtistCard(){
+export default function ArtistCard(props){
     return(
-            <Square bgImg="/assets/artists.jpg" bgSize="100%" aspectRatio="1">
-                1
+            <Square bgImg={props.img} bgSize="100%" aspectRatio="1">
+                <Flex w="100%" h="100%" flexDirection="column" p="5%" justifyContent="end">
+                    <Text mt="80%" textStyle="detail">{props.date}</Text>
+                    <Text textStyle="basic">{props.name}</Text>
+                </Flex>
             </Square>
     )
 }
