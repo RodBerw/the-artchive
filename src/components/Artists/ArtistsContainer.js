8d6656204917d7f067cc0df6aea39d3b79bbc2d5
Rodolfo <rodolfo.b.liberado@gmail.com>
2023-09-26 21:15:52 -0300

    json fix 1.0

--- src/components/Artists/ArtistsContainer.js
+++ src/components/Artists/ArtistsContainer.js
@@ -3,36 +3,47 @@ import ArtistCard from "./ArtistCard";
 import { Get } from "../../services/apiService";
 import { useEffect, useState } from "react";
 
-export default function ArtisitsContainer(){
-    const [data, setData] = useState([]);
-    const url = "./artistsDB.json"
-    const variant = useBreakpointValue(
-        {
-            base:"repeat(auto-fill, minmax(150px,1fr))",
-            md:"repeat(auto-fill, minmax(200px,1fr))",
-            xl:"repeat(auto-fill, minmax(15vw,1fr))"
-        },
-        {
-            fallback:"md",
-        },
-    )
+export default function ArtisitsContainer() {
+  const [data, setData] = useState([]);
+  const url =
+    "E:/Area de Trabalho Discreta/Programação/TheArtchive/the-artchive/src/components/Artists/artistsDB.json";
+  const variant = useBreakpointValue(
+    {
+      base: "repeat(auto-fill, minmax(150px,1fr))",
+      md: "repeat(auto-fill, minmax(200px,1fr))",
+      xl: "repeat(auto-fill, minmax(15vw,1fr))",
+    },
+    {
+      fallback: "md",
+    }
+  );
 
-    useEffect(() => {
-        setData(Get(url))
-        console.log(data)
-    },[data]);
+  useEffect(() => {
+    setData(Get(url));
+    console.log(data);
+  }, []);
 
-    return(
-        <div>
-            <Flex w="80%" bg="black" flexDirection="column" m="auto" zIndex="5">
-                <Grid w="100%" p="5%" gap="1vw" flexWrap="wrap" gridTemplateColumns={variant}>
-                    {data.map((artist,i) => (
-                        <div>
-                            <ArtistCard img={artist.img} date={artist.date} name={artist.name}/>
-                        </div>
-                    ))}
-                </Grid>
-            </Flex>
-        </div>
-    )
+  return (
+    <div>
+      <Flex w="80%" bg="black" flexDirection="column" m="auto" zIndex="5">
+        <Grid
+          w="100%"
+          p="5%"
+          gap="1vw"
+          flexWrap="wrap"
+          gridTemplateColumns={variant}
+        >
+          {/* {data.artists.map((artist, i) => (
+            <div>
+              <ArtistCard
+                img={artist.img}
+                date={artist.date}
+                name={artist.name}
+              />
+            </div>
+          ))} */}
+        </Grid>
+      </Flex>
+    </div>
+  );
 }
